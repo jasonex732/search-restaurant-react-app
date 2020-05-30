04292a85314bff07bfa8056514d6af9a99955b10
Jason Eccles <jasonxeccles@gmail.com>
2020-05-30 08:41:49 -0400

    Updates

---
+++ precache-manifest.1b97ccb42cc33fa25258c7e764ed34c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152f53ecdafdbb93912a100b36f3c9f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a267c89fc5d95795ba5",
+    "url": "/static/css/main.79a08113.chunk.css"
+  },
+  {
+    "revision": "abe44d01d552c29147d9",
+    "url": "/static/js/2.0dc263be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0dc263be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a267c89fc5d95795ba5",
+    "url": "/static/js/main.42e69f12.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
